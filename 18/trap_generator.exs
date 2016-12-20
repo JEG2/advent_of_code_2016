d6f3cd90f62a1b405a0b384204eb1640c1a98364
James Edward Gray II <james@graysoftinc.com>
2016-12-19 18:10:46 -0600

    Day 18.

---
+++ 18/trap_generator.exs
@@ -0,0 +1,48 @@
+defmodule TrapGenerator do
+  def generate(input) do
+    input
+    |> parse
+    |> wrap
+    |> build
+    |> count
+  end
+
+  defp parse(line) do
+    String.trim(line)
+  end
+
+  defp wrap(first_line) do
+    Stream.iterate(first_line, fn previous_line ->
+      ["."] ++ String.graphemes(previous_line) ++ ["."]
+      |> Enum.chunk(3, 1)
+      |> Enum.map(&generate_tile/1)
+      |> Enum.join
+    end)
+  end
+
+  defp generate_tile(["^", ".", "."]), do: "^"
+  defp generate_tile(["^", "^", "."]), do: "^"
+  defp generate_tile([".", "^", "^"]), do: "^"
+  defp generate_tile([".", ".", "^"]), do: "^"
+  defp generate_tile(_pattern), do: "."
+
+  defp build(generator) do
+    Enum.take(generator, 40)
+  end
+
+  defp count(rows) do
+    Enum.reduce(rows, 0, fn row, sum ->
+      new_count =
+        row
+        |> String.graphemes
+        |> Enum.count(fn tile -> tile == "." end)
+      sum + new_count
+    end)
+  end
+end
+
+System.argv
+|> hd
+|> File.read!
+|> TrapGenerator.generate
+|> IO.puts
